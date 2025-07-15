fe7a2f3d10255380ab738eb223b37af406ff81f9
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-07-15 02:34:18 +0200

    Add hash calculating util

---
+++ src/utils/hash_calculator.py
@@ -0,0 +1,23 @@
+"""Utility for calculating hash of files."""
+
+import hashlib
+
+from aiopath import AsyncPath  # type: ignore
+
+
+async def get_file_hash(file_path: AsyncPath, chunk_size: int = 65536) -> str:
+    """
+    Calculate the SHA-256 hash of a file asynchronously.
+
+    Args:
+        file_path: The path to the file to be hashed.
+        chunk_size: The number of bytes to read at once. Default is 64 KiB.
+
+    Returns:
+        The SHA-256 hash of the file as a hexadecimal string.
+    """
+    hash_sha256 = hashlib.sha256()
+    async with file_path.open("rb") as f:
+        while chunk := await f.read(chunk_size):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
